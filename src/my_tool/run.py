5391d5b1f4eaa20f36e0215e3a9df80069e36e7f
Prometheus <toshiyasato219@gmail.com>
2026-02-01 23:13:23 +0900

    feat: implement tree gen in side-panel display using txt file
    - several fixes needed like git pattern matchng e.g., "*","/", etc.

--- src/my_tool/run.py
+++ src/my_tool/run.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+import os 
+import subprocess
+import tempfile
 
 from .path_reader import generate_tree
 from .read_gitignore import get_top_level
@@ -19,15 +22,17 @@ def start():
     ignore = get_top_level(folder_to_scan)
 
 
-    # Start the process
-    print(folder_to_scan.name + "/")
-    # print(ignore)
-
     dir_tree = generate_tree(folder_to_scan, ignore)
 
-    print("="*10, "vs loop ", "="*10)
-    for dir in dir_tree:
-        print(dir)
+    temp_path = os.path.join(tempfile.gettempdir(), "tree_output.txt")
+    with open(temp_path, "w", encoding='utf-8') as f:
+        f.write(folder_to_scan.name + "/\n")
+        for dir in dir_tree:
+            f.write(dir + "\n")
+    
+    # 3. Tell VS Code to open it in a new column
+    # '-r' opens it in the current window, '2' opens it in the second column
+    subprocess.run(["code", "-r", "--column", "2", temp_path], shell=True)
 
 if __name__ == '__main__':
     start()
